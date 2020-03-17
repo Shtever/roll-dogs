153a46008eb794dcf2456833ed7f0757985b3180
Stephen McMillen <Shtevetm@gmail.com>
2020-03-17 13:44:42 -0500

    added function to roll dice selection [quantity] number of times and console log each result, and total. TODO: add modifier to total.

--- roll.js
+++ roll.js
@@ -6,12 +6,15 @@ function rollEm() {
     if (isNaN(quant) || isNaN(dice) || isNaN(mod)) {
         alert("Fill out all fields");
     } else {
-        var rollRaw = Math.floor((Math.random()) * dice) + 1
-        // TO DO 'rollRaw' x 'quant' number of times
+        var rollTotal = 0
+        for (var i = 0; i < quant; i++) {
+            var rollRaw = Math.floor((Math.random()) * dice) + 1
+            console.log("Die " + (i + 1) + ":" + rollRaw);
+            rollTotal = rollTotal + rollRaw
+        };
+        console.log("TOTAL: " + rollTotal);
         console.log("quant " + quant);
         console.log("dice " + dice);
         console.log("mod " + mod);
-        console.log("raw: " + rollRaw);
-        console.log("total: " + rollRaw);
     }
 }
