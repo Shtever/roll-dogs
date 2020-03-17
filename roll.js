6b7d3066fbe9ecef0a8ce3aaef0268d72120baa1
Stephen McMillen <Shtevetm@gmail.com>
2020-03-17 13:09:30 -0500

    function for grabbing inputs is working. TODO: function to roll selected dice X number of times.

--- roll.js
+++ roll.js
@@ -1,10 +1,17 @@
-rollDice(20, 1)
-
-function rollDice(dice, mod) {
-    var roll = (Math.floor((Math.random() * dice) + 1) + mod);
-    console.log(roll)
-    var total = roll + mod;
-    console.log("flat: " + roll);
-    console.log("total: " + total);
-
+function rollEm() {
+    var quant = parseInt(document.getElementById("quantity").value);
+    var dice = parseInt(document.getElementById("dice").value);
+    var mod = parseInt(document.getElementById("mod").value);
+    // event.preventDefault();
+    if (isNaN(quant) || isNaN(dice) || isNaN(mod)) {
+        alert("Fill out all fields");
+    } else {
+        var rollRaw = Math.floor((Math.random()) * dice) + 1
+        // TO DO 'rollRaw' x 'quant' number of times
+        console.log("quant " + quant);
+        console.log("dice " + dice);
+        console.log("mod " + mod);
+        console.log("raw: " + rollRaw);
+        console.log("total: " + rollRaw);
+    }
 }
