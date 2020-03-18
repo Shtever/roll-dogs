2855f6ab4e5ace3dedb6d22882ce4f04660d3dd6
Stephen McMillen <Shtevetm@gmail.com>
2020-03-17 22:41:03 -0500

    Everything works. Results render in this format: Rolling X dY +Z | TOTAL | *d1* *d2* *d3* TODO - OTHER DICE DENOMINATION

--- roll.js
+++ roll.js
@@ -3,12 +3,20 @@ function rollEm() {
     var dice = parseInt(document.getElementById("dice").value);
     var mod = parseInt(document.getElementById("mod").value);
     // event.preventDefault();
-    if (isNaN(quant) || isNaN(dice) || isNaN(mod)) {
+    if ((document.getElementById("dice").value) === "other") {
+        console.log("other")
+    } else if (isNaN(quant) || isNaN(dice)) {
         alert("Fill out all fields");
     } else {
+        document.getElementById("result").innerHTML= "";
+        document.getElementById("rolling").innerHTML= "";
+        document.getElementById("diceRolls").innerHTML= "";
         var rollTotal = 0
+        document.getElementById("rolling").append("Rolling " + quant + " d" + dice + " + " + mod);
+        document.getElementById("result").innerHTML= "<hr>"
         for (var i = 0; i < quant; i++) {
-            var rollRaw = Math.floor((Math.random()) * dice) + 1
+            var rollRaw = Math.floor((Math.random()) * dice) + 1;
+            document.getElementById("diceRolls").append(" *" + rollRaw + "* ");
             console.log("Die " + (i + 1) + ":" + rollRaw);
             rollTotal = rollTotal + rollRaw
         };
@@ -16,5 +24,6 @@ function rollEm() {
         console.log("quant " + quant);
         console.log("dice " + dice);
         console.log("mod " + mod);
+        document.getElementById("result").append("TOTAL: " + rollTotal);
     }
 }
